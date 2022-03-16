26e525e8a1a16ca37922ce47147d2d8073eb5626
JulioVianaDev <juliovianadev@gmail.com>
2022-03-15 22:59:45 -0300

    terminando o exemplo

---
+++ src/components/grid_class/index.jsx
@@ -0,0 +1,30 @@
+import './grid_class.css';
+
+const Exemplo_Grid_class = ()=>{
+    return(
+        <>
+            <div className="container">
+                <div className="item primeiro">
+                    1
+                </div>
+                <div className="item segundo">
+                    2
+                </div>
+                <div className="item terceiro">
+                    3
+                </div>
+                <div className="item quarto">
+                    4
+                </div>
+                <div className="item quinto ">
+                    5
+                </div>
+                <div className="item sexto">
+                    6
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Exemplo_Grid_class;
